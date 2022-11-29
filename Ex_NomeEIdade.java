a0d34b6cbb356e5aed2df7b0690d18057e87208a
Shirley Lozano <98967475+Shirleytea@users.noreply.github.com>
2022-11-29 17:18:06 -0300

    Update Ex_NomeEIdade.java

--- Ex_NomeEIdade.java
+++ Ex_NomeEIdade.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class Ex_NomeEIdade {
-
+  
+      /*Faça um programa que leia conjuntos de dois valores
+ * o primeiro, representando o nome do aluno e o segundo, 
+ * representando a sua idade.*/
 
 public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
+Scanner scan = new Scanner(System.in);
 
-      String nome;
-      int idade;
+String nome;
+int idade;
       
         
-          while (true) {
-            System.out.println("Nome:");
-            nome = scan.next();
-            if (nome.equals("0")) break;
-            System.out.println("idade:");
-            idade = scan.nextInt();
-          }
-            
-          
-      
+   while (true) {
+       System.out.println("Nome:");
+       nome = scan.next();
+       if (nome.equals("0")) break;
+       System.out.println("idade:");
+       idade = scan.nextInt();
+      }
+               
 }     
 }
